3a84b7c5bd55b20c28e9c0227ed789047214b0f9
Daniel Watson <waniel@watson.danielwilliamwatson.com>
2017-08-05 13:06:35 -0500

    Fixing indentation.

--- country_search/src/CountryInputForm/CountryInputForm.js
+++ country_search/src/CountryInputForm/CountryInputForm.js
@@ -2,48 +2,48 @@ import React, { Component } from 'react';
 
 class CountryInputForm extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true
-    }
-  }
-
-  componentDidMount() {
-    const url = 'http://localhost:8000';
-    let data = {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: "query=col"
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLoading: true
+        }
     }
 
-    return fetch(url, data)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      this.setState({                     // Funtion setState call rerenders Component...
-        isLoading: false,
-        countryData: responseJson
-      });
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    componentDidMount() {
+        const url = 'http://localhost:8000';
+        let data = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: "query=col"
+        }
+
+        return fetch(url, data)
+        .then((response) => response.json())
+        .then((responseJson) => {
+            this.setState({                     // Funtion setState call rerenders Component...
+                isLoading: false,
+                countryData: responseJson
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
   }
 
 
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <p>Waiting for data!</p>
-      );
+    render() {
+        if (this.state.isLoading) {
+            return (
+              <p>Waiting for data!</p>
+            );
+        }
+        return (
+            <p>{JSON.stringify(this.state.countryData)}</p>
+        );
     }
-    return (
-      <p>{JSON.stringify(this.state.countryData)}</p>
-    );
-  }
 }
 
 export default CountryInputForm;
