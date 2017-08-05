e74c03dec60faf9846fffed66acece3e8a08e5e7
Daniel Watson <waniel@watson.danielwilliamwatson.com>
2017-08-05 13:44:26 -0500

    This will now be the base state of master branch for further dev.

--- country_search/src/CountryInputForm/CountryInputForm.js
+++ country_search/src/CountryInputForm/CountryInputForm.js
@@ -4,44 +4,17 @@ export default class CountryInputForm extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-          isLoading: true
-        }
     }
 
     componentDidMount() {
-        const url = 'http://localhost:8000';
-        let data = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: "query=col"
-        }
 
-        return fetch(url, data)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            this.setState({                     // Funtion setState call rerenders Component...
-                isLoading: false,
-                countryData: responseJson
-            });
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-  }
-
+    }
 
     render() {
-        if (this.state.isLoading) {
-            return (
-              <p>Waiting for data!</p>
-            );
-        }
         return (
-            <p>{JSON.stringify(this.state.countryData)}</p>
+            <form>
+                <input type='text' placeholder='Country Name, A2 Code or A3 Code:'/>
+            </form>
         );
     }
 }
